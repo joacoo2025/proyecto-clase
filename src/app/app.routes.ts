e28f7fc6af77f8c2419d369b66ab1c367772e45b
joacoo2025 <joaquifelimu@gmail.com>
2025-04-08 11:02:09 -0300

    creacion de productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './start/inicio/inicio.component';
 import { ContactoComponent } from './formulario/contacto/contacto.component';
+import { ProductosComponent } from './producto/productos/productos.component';
 
 export const routes: Routes = [
     {path: '', redirectTo:'/inicio',pathMatch:'full'},
     {path:'inicio',component:InicioComponent},
     {path:'contacto',component:ContactoComponent},
+    {path:'productos',component:ProductosComponent},
 ];
